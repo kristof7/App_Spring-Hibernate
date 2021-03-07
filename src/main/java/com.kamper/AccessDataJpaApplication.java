7c47d30d4f18424dc2c120f53deb75de0aff8fd4
Krzysztof Kampa <krzysztofkampa7@gmail.com>
2021-03-07 12:09:14 +0100

    Create Spring project structure with hibernate

---
+++ src/main/java/com.kamper/AccessDataJpaApplication.java
@@ -0,0 +1,16 @@
+package com.kamper;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class AccessDataJpaApplication {
+
+        public static void main(String[] args) {
+            SpringApplication.run(AccessDataJpaApplication.class, args);
+
+        }
+
+}
